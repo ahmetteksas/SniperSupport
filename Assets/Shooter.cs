4a39c63c4bcebaba88bea1278c6eb57e6ef30ccb
ahmetteksas <63156483+ahmetteksas@users.noreply.github.com>
2022-01-07 15:36:34 +0300

    Healthimplemented

--- Assets/Shooter.cs
+++ Assets/Shooter.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
     public Transform bulletSpawnPos;
+    Transform character;
+    Transform ai;
     public float bulletForce;
     private float shootDelay = 1.5f;
     private float shootTime = 0f;
     void Start()
     {
+        if (this.gameObject.name =="Character")
+        {
+            character = transform;
+        }
+        else
+        {
+            ai = transform;
+            ai.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, -character.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+        }
         //shootDelay = shootTime - Time.deltaTime;
     }
 
@@ -24,12 +35,23 @@ public class Shooter : MonoBehaviour
     }
     public void Shoot()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonUp(0))
         {
-           GameObject _smallbullet= ObjectPool.instance.SpawnFromPool("BulletSmall", bulletSpawnPos.position, Quaternion.identity);
-            _smallbullet.transform.rotation = transform.rotation;
-           _smallbullet.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * bulletForce);
-            shootTime = 0f;
+            if (this.gameObject.name == "character")
+            {
+                GameObject _smallbullet = ObjectPool.instance.SpawnFromPool("BulletSmallPlayer", bulletSpawnPos.position, Quaternion.identity);
+                _smallbullet.transform.rotation = character.rotation;
+                _smallbullet.gameObject.GetComponent<Rigidbody>().AddForce(character.forward * bulletForce);
+                shootTime = 0f;
+            }
+            else
+            {
+                GameObject _smallbullet = ObjectPool.instance.SpawnFromPool("BulletSmallPlayer", bulletSpawnPos.position, Quaternion.identity);
+                _smallbullet.transform.rotation = ai.rotation;
+                _smallbullet.gameObject.GetComponent<Rigidbody>().AddForce(ai.forward * bulletForce);
+                shootTime = 0f;
+            }
+           
         }
     }
 }
--- Assets/SoldierController.cs
+++ Assets/SoldierController.cs
@@ -1,31 +1,66 @@
+using ScriptableObjectArchitecture;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
+using System.Linq;
+
 
 public class SoldierController : MonoBehaviour
 {
+    [SerializeField] IntVariable enemyCount;
+    [SerializeField] IntVariable allyCount;
+
+    [SerializeField] GameObjectCollection enemyList;
+    [SerializeField] GameObjectCollection allyList;
+
     public Transform bulletSpawnPos;
+
     public float bulletForce;
     private float shootDelay = .5f;
     private float shootTime = 0f;
+
     private ProgressBarPro health;
+
+
+    private void Awake()
+    {
+        enemyCount.Value = 0;
+        allyCount.Value = 0;
+        enemyList.Clear();
+        allyList.Clear();
+    }
     void Start()
     {
+        if (this.gameObject.tag == "Enemy")
+        {
+            enemyList.Add(this.gameObject);
+            enemyCount.Value ++;
+        }
+        if (this.gameObject.tag == "Ally")
+        {
+            allyList.Add(this.gameObject);
+            allyCount.Value++;
+        }
         health = GetComponentInChildren<ProgressBarPro>();
+        
         //shootDelay = shootTime - Time.deltaTime;
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.CompareTag("BulletSmall")&&this.gameObject.tag =="Ally")
+        if ((other.gameObject.CompareTag("BulletSmall") || other.gameObject.CompareTag("BulletSmallPlayer"))&&this.gameObject.tag =="Ally")
         {
             health.Value -= .02f;
         }
         if (other.gameObject.CompareTag("BulletSmall") && this.gameObject.tag == "Enemy")
         {
             health.Value -= .01f;
+        }
+        if (other.gameObject.CompareTag("BulletSmallPlayer") &&this.gameObject.tag == "Enemy")
+        {
+            health.Value -= .1f;
         }
     }
     void Update()
@@ -35,8 +70,16 @@ public class SoldierController : MonoBehaviour
         {
             Shoot();
         }
-        if (health.Value ==0)
+        if (health.Value ==0 && this.gameObject.tag =="Enemy")
         {
+            enemyList.Remove(this.gameObject);
+            enemyCount.Value--;
+            gameObject.SetActive(false);
+        }
+        if (health.Value == 0 && this.gameObject.tag == "Ally")
+        {
+            allyList.Remove(this.gameObject);
+            allyCount.Value--;
             gameObject.SetActive(false);
         }
     }
