ecb578f32cbf856ffc58ab1f5e7ff8e177f789c3
ahmetteksas <63156483+ahmetteksas@users.noreply.github.com>
2022-01-16 19:08:13 +0300

    HitParticleMetaV1

--- Assets/_PROJECT_/Scripts/Bullet/BulletController.cs
+++ Assets/_PROJECT_/Scripts/Bullet/BulletController.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class BulletController : MonoBehaviour
 {
     public float damage;
+    public GameObject impact;
     void Start()
     {
         StartCoroutine(DestroyObject());
     }
-
+    private void OnCollisionEnter(Collision other)
+    {
+        impact.SetActive(true);
+        impact.GetComponent<ParticleSystem>().Play();
+    }
     void Update()
     {
         
     }
+    
     IEnumerator DestroyObject()
     {
         yield return new WaitForSeconds(1f);
