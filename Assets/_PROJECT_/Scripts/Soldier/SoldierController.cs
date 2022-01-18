ed96edf1c868c448c6e35251b98667233c0ff6e1
ahmetteksas <63156483+ahmetteksas@users.noreply.github.com>
2022-01-18 22:51:28 +0300

    TrailTryMeta2

--- Assets/_PROJECT_/Scripts/Soldier/SoldierController.cs
+++ Assets/_PROJECT_/Scripts/Soldier/SoldierController.cs
@@ -189,12 +189,12 @@ public class SoldierController : MonoBehaviour
                         _smallBullet = ObjectPool.instance.SpawnFromPool("BulletRocket", bulletSpawnPos.position - _offset, Quaternion.identity);
                     }
                 }
-                //_smallBullet.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * bulletForce);
                 if (targetEnemy)
                 {
-                    _smallBullet.transform.position = Vector3.MoveTowards(_smallBullet.transform.position, targetEnemy.transform.position, 40f * Time.deltaTime);
+                    _smallBullet.transform.LookAt(targetTransform);
+                    _smallBullet.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * bulletForce);
+                    //_smallBullet.transform.position = Vector3.MoveTowards(_smallBullet.transform.position, targetEnemy.transform.position, 40f * Time.deltaTime);
                 }
-                //_smallBullet.transform.LookAt(targetTransform);
             }
         }
     }
