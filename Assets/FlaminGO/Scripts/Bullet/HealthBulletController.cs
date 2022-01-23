78514d536083c40456d1798723f1c010ebd9e7ff
ahmetteksas <63156483+ahmetteksas@users.noreply.github.com>
2022-01-23 23:33:34 +0300

    HealBulletMeta

---
+++ Assets/FlaminGO/Scripts/Bullet/HealthBulletController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBulletController : MonoBehaviour
+{
+    public float healValue = .2f;
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+            SoldierController _soldierController = other.gameObject.GetComponentInParent<SoldierController>();
+            if (_soldierController)
+            {
+                _soldierController.HealHit(healValue);
+            }
+            gameObject.SetActive(false);
+    }
+
+}
