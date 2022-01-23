c2823eb31d9ebd82f57cc20b146ee299e00313b8
hasankoklu <hasankoklu@hotmail.com.tr>
2022-01-23 21:37:27 +0300

    Swerve Demo 1.2.0

--- Assets/FlaminGO/Scripts/_Temp/SwerveMovement.cs
+++ Assets/FlaminGO/Scripts/_Temp/SwerveMovement.cs
@@ -7,6 +7,8 @@ public class SwerveMovement : MonoBehaviour
     public float rotSpeed = 3f;
 
 
+    public float xClampMin = 0f;
+    public float xClampMax = 38f;
     public float yClampMin = 230f;
     public float yClampMax = 267f;
     public float zClampMin = 230f;
@@ -26,10 +28,11 @@ public class SwerveMovement : MonoBehaviour
         //float _rotationX = Mathf.Clamp(transform.rotation.eulerAngles.x, -90.0F, 0.0F);
         // transform.rotation = Quaternion.Euler(rotationX, transform.eulerAngles.y, transform.eulerAngles.z);
         Vector3 rotation = transform.eulerAngles;
+        rotation.x = Mathf.Clamp(transform.eulerAngles.x, xClampMin, xClampMax);
         rotation.y = Mathf.Clamp(transform.eulerAngles.y, yClampMin, yClampMax);
         rotation.z = Mathf.Clamp(transform.eulerAngles.z, zClampMin, zClampMax);
 
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, rotation.y, rotation.z);
+        transform.rotation = Quaternion.Euler(rotation.x, rotation.y, rotation.z);
         float distance = zClampMax - transform.eulerAngles.z;
 
         //shooter.scopeZoom = defaultScopeZoom * (distance) / (zClampMax - zClampMin);
@@ -39,7 +42,7 @@ public class SwerveMovement : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
-            transform.localEulerAngles += rotSpeed * new Vector3(0f, Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            transform.localEulerAngles += rotSpeed * new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
         }
     }
 }
