112104bcfd65211e78c2a3ec32b9502cc3f9863d
hasankoklu <hasankoklu@hotmail.com.tr>
2022-01-26 02:22:15 +0300

    meta

--- Assets/HSDisable.cs
+++ Assets/HSDisable.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class HSDisable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     private void OnEnable()
     {
         StartCoroutine(DisableObject());
